e3abc0d99299c555ba823819420a9f409afb9631
Sazzad Hossain <hossain.sakib26@hotmail.com>
2021-07-04 21:58:46 +0600

    first commit

---
+++ My_CoreApp_MicrosoftDoc/Data/My_CoreApp_MicrosoftDocContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using My_CoreApp_MicrosoftDoc.Models;
+
+namespace My_CoreApp_MicrosoftDoc.Data
+{
+    public class My_CoreApp_MicrosoftDocContext : DbContext
+    {
+        public My_CoreApp_MicrosoftDocContext (DbContextOptions<My_CoreApp_MicrosoftDocContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ My_CoreApp_MicrosoftDoc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_CoreApp_MicrosoftDoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
