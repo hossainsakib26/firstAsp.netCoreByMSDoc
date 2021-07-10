10d74c3baaf89afe84fca28aa24b0aa642c70a2d
Sazzad Hossain <hossain.sakib26@hotmail.com>
2021-07-10 07:50:31 +0600

    ms document code for mvc core done

--- My_CoreApp_MicrosoftDoc/Migrations/20210710012654_addNewFieldInMovie.Designer.cs
+++ My_CoreApp_MicrosoftDoc/Migrations/20210710012654_addNewFieldInMovie.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using My_CoreApp_MicrosoftDoc.Data;
 
 namespace My_CoreApp_MicrosoftDoc.Migrations
 {
     [DbContext(typeof(My_CoreApp_MicrosoftDocContext))]
-    partial class My_CoreApp_MicrosoftDocContextModelSnapshot : ModelSnapshot
+    [Migration("20210710012654_addNewFieldInMovie")]
+    partial class addNewFieldInMovie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
